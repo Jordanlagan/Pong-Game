c2e71175a618e9e07a33969176f8e0bebccf6683
Jordan Lagan <jordanlagan000@gmail.com>
2017-11-30 19:09:23 -0500

    working on step and animate functions

--- game.js
+++ game.js
@@ -13,26 +13,34 @@ function Ball(x, y, radius) {
     this.radius = radius
 }
 
+var canvas = document.getElementById("room0");
+var context = canvas.getContext("2d");
+
+Ball.prototype.render = function() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+    context.stroke();
+}
+
+Paddle.prototype.render = function() {
+    context.fillRect(this.x, this.y, this.width, this.height);
+}
+
 var player = new Paddle(5, 225);
 var computer = new Paddle(625, 225);
 var ball = new Ball(320, 240, 5);
 
 // Render
 
-var drawRect = function(object, context) {
-    return context.fillRect(object.x, object.y, object.width, object.height);
-}
-
-var drawCircle = function(object, context) {
-    context.beginPath();
-    context.arc(object.x, object.y, object.radius, 0, 2*Math.PI);
-    return context.stroke();
-}
-
 var render = function() {
-    var canvas = document.getElementById("room0");
-    var context = canvas.getContext("2d");
-    drawRect(player, context);
-    drawRect(computer, context);
-    drawCircle(ball, context);
+    player.render();
+    computer.render();
+    ball.render();
 }
+
+// Animate
+var step = function() {
+    render();
+}
+
+var animate = window.requestAnimationFrame || function(step) { window.setTimeout(step, 1000/60) };
